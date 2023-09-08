539ca782e78dc7b9801a32e2fa1484bb43e1cd82
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2023-09-08 15:08:15 -0300

    agora as imagens estao certas

--- src/Produtos.jsx
+++ src/Produtos.jsx
@@ -1,4 +1,8 @@
 import './assets/produtos.css'
+import cachorroImagem from './assets/cachorro.jpg'
+import elefanteImagem from './assets/elefante.jpg'
+import focaImagem from './assets/foca.jpg'
+import macacoImagem from './assets/macaco.jpg'
 
 function Produtos() {
     return (
@@ -6,22 +10,22 @@ function Produtos() {
             <div className="mainContainer">
                 <div className="animais">
                     <div className="animal">
-                        <img src="./src/assets/cachorro.jpg" alt="Um cachorro" />
+                        <img src={cachorroImagem} alt="Um cachorro" />
                         <h1>Cachorro</h1>
                         <p>R$ 368,99</p>
                     </div>
                     <div className="animal">
-                        <img src="./src/assets/elefante.jpg" alt="Um elefante" />
+                        <img src={elefanteImagem} alt="Um elefante" />
                         <h1>Elefante</h1>
                         <p>R$ 5800,00</p>
                     </div>
                     <div className="animal">
-                        <img src="./src/assets/foca.jpg" alt="Uma foca" />
+                        <img src={focaImagem} alt="Uma foca" />
                         <h1>Foca</h1>
                         <p>R$ 1325,00</p>
                     </div>
                     <div className="animal">
-                        <img src="./src/assets/macaco.jpg" alt="Um macaco" />
+                        <img src={macacoImagem} alt="Um macaco" />
                         <h1>Macaco</h1>
                         <p>R$ 189,99</p>
                     </div>
--- src/Sobre.jsx
+++ src/Sobre.jsx
@@ -1,11 +1,12 @@
 import './assets/sobre.css'
+import tigreImagem from './assets/tigre.jpg'
 
 function Sobre() {
     return (
         <>
             <div className="mainContainerSobre">
                 <div className="boxSobre">
-                    <img src="./src/assets/tigre.jpg" alt="Um tigre" />
+                    <img src={tigreImagem} alt="Um tigre" />
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam error aspernatur commodi, praesentium ex odio? Magnam aliquid rem vero eveniet!</p>
                 </div>
             </div>
