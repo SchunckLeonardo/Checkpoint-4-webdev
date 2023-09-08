83267d129c8b59eb0eb3c75aeebb160bb72fdda5
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2023-09-08 14:48:13 -0300

    concertando as imgs

--- src/Sobre.jsx
+++ src/Sobre.jsx
@@ -5,7 +5,7 @@ function Sobre() {
         <>
             <div className="mainContainerSobre">
                 <div className="boxSobre">
-                    <img src="./assets/tigre.jpg" alt="Um tigre" />
+                    <img src="./src/assets/tigre.jpg" alt="Um tigre" />
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam error aspernatur commodi, praesentium ex odio? Magnam aliquid rem vero eveniet!</p>
                 </div>
             </div>
